c95bc71406ad1498864468c78ffd9d9f1c9cd530
timcmiller <timcmiller@live.com>
2016-02-17 12:23:27 -0800

    inital commit

---
+++ main.js
@@ -0,0 +1,121 @@
+/**
+ * This is the entry point for your experience that you will run on Exponent.
+ *
+ * Start by looking at the render() method of the component called
+ * FirstExperience. This is where the text and example components are.
+ */
+'use strict';
+
+let React = require('react-native');
+let {
+  Animated,
+  AppRegistry,
+  Easing,
+  Image,
+  ScrollView,
+  StatusBarIOS,
+  StyleSheet,
+  ListView,
+  Text,
+  Component,
+  View,
+} = React;
+
+let REQUEST_URL = 'http://beer-database.herokuapp.com/api/beers';
+let LOGO = require('./images/logo.jpg');
+
+let ExBoxes = require('./ExBoxes');
+let ExPhotoGallery = require('./ExPhotoGallery');
+let ExScreen = require('./ExScreen');
+
+class AwesomeProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
+        });
+      })
+      .done();
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.listView}/>
+    );
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={LOGO}/>
+        <Text>
+          Tapping the keg...
+        </Text>
+      </View>
+    );
+  }
+
+  renderMovie(movie) {
+    return (
+      <View style={styles.container}>
+        Yum, beer
+      </View>
+    );
+  }
+}
+
+let styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+AppRegistry.registerComponent('main', () => AwesomeProject);
